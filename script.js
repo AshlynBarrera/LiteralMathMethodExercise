7280281cfdbb7f82afc1cc5a2ffd9602fa7cb64d
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-01-20 10:52:43 -0600

    Created LiteralMathMEthodExercise folder and script.js file git rm

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+
+
+//1a
+let warmHugs = "Hi, I'm Olaf and I like warm hugs."
+
+//1b
+console.log(warmHugs.toUpperCase());
+//1c
+
+console.log(warmHugs.replace("like","love"));
+//2a
+var beenImpaled = "Oh, look at that. I've been impaled."
+
+//2b
+console.log(beenImpaled.slice(18));
+
+//3a
+var dotDotDot = "..."
+
+//3b
+var skullBones = 'I don\'t have a skull' + dotDotDot + "or bones."
+console.log(skullBones);
+
+//4
+console.log(Math.PI);  
+
+//5 
+var randomNumber = Math.random();
+console.log(randomNumber);
+randomNumber *= 4;
+randomNumber = Math.floor(randomNumber);
+console.log(randomNumber);
+
+
+
+
+
+
+
+
+
